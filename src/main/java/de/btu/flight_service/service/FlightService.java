01272468784d7b6849fefef1ea2a088d31f394ec
Mikhail Stepanov <heirloon@mail.ru>
2019-12-18 17:42:16 +0300

    commit

--- src/main/java/de/btu/flight_service/service/FlightService.java
+++ src/main/java/de/btu/flight_service/service/FlightService.java
@@ -32,7 +32,7 @@ import java.util.*;
 public class FlightService implements IFlightService {
 
     @Value("${spring.elasticsearch.rest.uris}")
-    private String host = "localhost";
+    private String host = "84.201.186.8";
 
     @Value("${elasticsearch.index")
     private String index;
